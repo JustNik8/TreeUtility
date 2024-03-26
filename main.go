44afac38302f048d60ea564ba07daf73df3d7045
JustNik <nikita-999999@yandex.ru>
2024-03-26 22:45:49 +0300

    implement tree utility

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -19,6 +21,110 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
+	return dfsTree(out, path, printFiles, 0, 0)
+}
+
+func dfsTree(out io.Writer, path string, printFiles bool, lvl int, prevNotLastCnt int) error {
+	openedEntries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	var entries []os.DirEntry
+	if printFiles {
+		entries = make([]os.DirEntry, len(openedEntries))
+		copy(entries, openedEntries)
+	} else {
+		for i := range openedEntries {
+			info, err := openedEntries[i].Info()
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				entries = append(entries, openedEntries[i])
+			}
+		}
+	}
+
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Name() < entries[j].Name()
+	})
+
+	for i, e := range entries {
+		isLast := len(entries)-1 == i
+		err := printGraphics(out, lvl, isLast, printFiles, prevNotLastCnt, e)
+		if err != nil {
+			return err
+		}
+
+		if e.IsDir() {
+			newPath := path + string(os.PathSeparator) + e.Name()
+			currNotLastCnt := prevNotLastCnt
+			if !isLast {
+				currNotLastCnt++
+			}
+			err = dfsTree(out, newPath, printFiles, lvl+1, currNotLastCnt)
+			if err != nil {
+				return err
+			}
+		}
+	}
 
 	return nil
+}
+
+func printGraphics(out io.Writer, lvl int, isLast bool, printFiles bool, prevNotLastCnt int, e os.DirEntry) error {
+	if !e.IsDir() && !printFiles {
+		return nil
+	}
+
+	for i := 0; i < lvl; i++ {
+		if i < prevNotLastCnt {
+			_, err := out.Write([]byte("│"))
+			if err != nil {
+				return err
+			}
+		}
+
+		_, err := out.Write([]byte("\t"))
+		if err != nil {
+			return err
+		}
+	}
+
+	var info string
+	var err error
+	if e.IsDir() {
+		info = e.Name()
+	} else {
+		info, err = getFileInfo(e)
+		if err != nil {
+			return err
+		}
+	}
+
+	if isLast {
+		str := "└───" + info + "\n"
+		_, err = out.Write([]byte(str))
+	} else {
+		str := "├───" + info + "\n"
+		_, err = out.Write([]byte(str))
+	}
+	return err
+}
+
+func getFileInfo(e os.DirEntry) (string, error) {
+	info, err := e.Info()
+	if err != nil {
+		return "", err
+	}
+
+	var size string
+	if info.Size() > 0 {
+		size = fmt.Sprintf("%db", info.Size())
+	} else {
+		size = "empty"
+	}
+	return fmt.Sprintf("%s (%s)", e.Name(), size), nil
 }
